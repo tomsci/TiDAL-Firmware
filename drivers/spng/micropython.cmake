bb9c2fa3752922feec01b1c3170ae74645b2a550
Tom Sutcliffe <tomsci@me.com>
2022-05-21 15:46:26 +0100

    cmake butchery

--- drivers/spng/micropython.cmake
+++ drivers/spng/micropython.cmake
@@ -6,17 +6,17 @@ add_library(usermod_spng INTERFACE)
 # Add our source files to the lib
 target_sources(usermod_spng INTERFACE
     ${CMAKE_CURRENT_LIST_DIR}/spng_wrapper.c
-    #${CMAKE_CURRENT_LIST_DIR}/../../libspng/spng/spng.c
-    ${CMAKE_CURRENT_LIST_DIR}/miniz.c
+    # Yes miniz has a cmake project. No I can't get it to work.
+    ${CMAKE_CURRENT_LIST_DIR}/../../miniz/miniz.c
+    ${CMAKE_CURRENT_LIST_DIR}/../../miniz/miniz_zip.c
+    ${CMAKE_CURRENT_LIST_DIR}/../../miniz/miniz_tinfl.c
+    ${CMAKE_CURRENT_LIST_DIR}/../../miniz/miniz_tdef.c
 )
 
 target_include_directories(usermod_spng INTERFACE
-    ${CMAKE_CURRENT_LIST_DIR}
+    ${CMAKE_CURRENT_LIST_DIR}/../../miniz
     ${CMAKE_CURRENT_LIST_DIR}/../../libspng/spng
 )
 
-
 # Link our INTERFACE library to the usermod target.
 target_link_libraries(usermod INTERFACE usermod_spng)
-
-#add_subdirectory("${CMAKE_CURRENT_LIST_DIR}/../../miniz" "${CMAKE_CURRENT_LIST_DIR}")
--- drivers/spng/spng_wrapper.c
+++ drivers/spng/spng_wrapper.c
@@ -1,3 +1,17 @@
 #define SPNG_USE_MINIZ
 // #include "spng.h"
 #include "../../libspng/spng/spng.c"
+
+#include "py/runtime.h"
+
+STATIC const mp_rom_map_elem_t spng_module_globals_table[] = {
+    { MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR_spng) },
+};
+STATIC MP_DEFINE_CONST_DICT(spng_module_globals, spng_module_globals_table);
+
+const mp_obj_module_t spng_user_module = {
+    .base = { &mp_type_module },
+    .globals = (mp_obj_dict_t *)&spng_module_globals,
+};
+
+MP_REGISTER_MODULE(MP_QSTR_spng, spng_user_module, 1);
