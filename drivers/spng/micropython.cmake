d9be7e002d69d54f6d012a9b11eeb66293c7f0b4
Tom Sutcliffe <tomsci@me.com>
2022-05-21 14:32:15 +0100

    Paths!

--- drivers/spng/micropython.cmake
+++ drivers/spng/micropython.cmake
@@ -5,12 +5,12 @@ add_library(usermod_spng INTERFACE)
 # Add our source files to the lib
 target_sources(usermod_spng INTERFACE
     ${CMAKE_CURRENT_LIST_DIR}/spng_wrapper.c
-    ${CMAKE_CURRENT_LIST_DIR}/../../libspng/spng.c
+    ${CMAKE_CURRENT_LIST_DIR}/../../libspng/spng/spng.c
 
 )
 
 target_include_directories(usermod_spng INTERFACE
-    ${CMAKE_CURRENT_LIST_DIR}/../../libspng
+    ${CMAKE_CURRENT_LIST_DIR}/../../libspng/spng
 )
 
 target_compile_definitions(usermod_spng INTERFACE
